e910c984e79dab3aec60208c4344f2985764d4b8
CAMILO <cestrad5@gmail.com>
2023-01-05 13:24:03 -0500

    Adding insertinon sort v2

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,44 +1,46 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers
- * in ascending order using the Insertion sort algorithm
- * @list: Double pointer to the list's head.
- * Return: void.
+ * swap - swaps two elements in an array
+ * @list: pointer to the head of the list
+ * @node1: pointer to the first node
+ * @node2: pointer to the second node
+ * Return: void
  */
+void swap(listint_t **list, listint_t **node1, listint_t *node2)
+{
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
+		*list = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
+}
 
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * @list: pointer to the head of the list
+ * Return: void
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *aux = NULL;
+	listint_t *prev, *next;
 
-	if (!list || !*list)
+	if (list == NULL || *list == NULL)
 		return;
-	aux = *list;
-	aux = aux->next;
-	while (aux)
+
+	for (next = (*list)->next; next != NULL; next = next->next)
 	{
-		while (aux->n < aux->prev->n)
+		prev = next->prev;
+		while (prev != NULL && prev->n > next->n)
 		{
-			aux->prev->next = aux->next;
-			if (aux->next)
-				aux->next->prev = aux->prev;
-			aux->next = aux->prev;
-			if (aux->prev->prev)
-			{
-				aux->prev = aux->prev->prev;
-				aux->next->prev = aux;
-				aux->prev->next = aux;
-			}
-			else
-			{
-				aux->prev = NULL;
-				aux->next->prev = aux;
-				*list = aux;
-				print_list(*list);
-				break;
-			}
+			swap(list, &prev, next);
 			print_list(*list);
 		}
-		aux = aux->next;
 	}
 }
